b8dd81f2bb4f6687124a88b276aba50631c8b14b
Aditya Jethliya <adityajet99@gmail.com>
2024-05-26 11:17:49 +0200

    Add support for more sounds. And change homepage to image

--- .venv/Scripts/app.py
+++ .venv/Scripts/app.py
@@ -6,27 +6,11 @@ from pydub.playback import play
 from PIL import Image
 
 # Function to call the command line program
-def process_audio(input_file, use_drum, use_beatnet):
+def process_audio(input_file, beat_type, detection_method):
     output_dir = "C:\\Users\\adars\\Desktop\\kickelhack"
     os.makedirs(output_dir, exist_ok=True)
-    cmd = ["C:\\Users\\adars\\PycharmProjects\\BeatVisualizer\\.venv\\Scripts\\python.exe", "C:\\Users\\adars\\PycharmProjects\\BeatVisualizer\\.venv\\Scripts\\beat_detection.py", input_file, output_dir]
-    if use_drum:
-        cmd.append("--use_drum")
-    if use_beatnet:
-        cmd.append("--use_beatnet")
+    cmd = ["C:\\Users\\adars\\PycharmProjects\\BeatVisualizer\\.venv\\Scripts\\python.exe", "C:\\Users\\adars\\PycharmProjects\\BeatVisualizer\\.venv\\Scripts\\beat_detection.py", input_file, output_dir, beat_type, detection_method]
     subprocess.run(cmd)
-
-
-# def process_audio(input_file, use_drum):
-#     output_dir = "output"
-#     os.makedirs(output_dir, exist_ok=True)
-#
-#     activate_venv = '.venv/bin/activate'
-#     cmd = f"{activate_venv} && python beat_detection.py {input_file} {output_dir}"
-#     if use_drum:
-#         cmd += " --use_drum"
-#
-#     subprocess.run(cmd, shell=True, check=True)
 
 # Function to display audio files and allow playing
 def display_audio(audio_file, index):
@@ -40,31 +24,26 @@ def display_image(image_file):
 
 # Function to download text file
 def download_text(text_file):
-    # with open(text_file, "r") as file:
-    #     text = file.read()
-    # st.download_button(label="Download Beat Locations", data=text, file_name="beat_locations.txt")
     with open(text_file, "r") as file:
         text = file.read()
     st.download_button(label="Download Beat Locations", data=text, file_name="beat_locations.txt", mime='text/plain')
 
 def main():
-    st.title("Beat Detection Application")
+    # st.title("Beat Detection Application")
+    st.image("C:\\Users\\adars\\PycharmProjects\\BeatVisualizer\\.venv\\Scripts\\banner.svg", use_column_width=True)
 
     # Upload audio file
     audio_file = st.file_uploader("Upload Audio File", type=["wav"])
 
-    # Checkbox to choose drums
-    use_drum = st.checkbox("Use Drums")
-
-    # Checkbox to choose beatnet
-    use_beatnet = st.checkbox("Use BeatNet")
+    beat_type = st.selectbox("Choose Beat Type", options=["Metronome", "Drum", "Meow", "Guitar", "Chicken"])
+    detection_method = st.selectbox("Choose Beat Detection Method", options=["Librosa", "BeatNet"])
 
     if audio_file:
         # Process button
         if st.button("Process"):
             with open("input.wav", "wb") as file:
                 file.write(audio_file.read())
-            process_audio("input.wav", use_drum, use_beatnet)
+            process_audio("input.wav", beat_type, detection_method)
 
             # Display outputs
             output_dir = "C:\\Users\\adars\\Desktop\\kickelhack"
@@ -73,12 +52,10 @@ def main():
             waveform_with_beats_image = os.path.join(output_dir, "waveform_with_beats.png")
             spectrogram_image = os.path.join(output_dir, "spectrogram.png")
             spectrogram_with_beats_image = os.path.join(output_dir, "spectrogram_with_beats.png")
-            metronome_audio = os.path.join(output_dir, "metronome.wav")
-            drum_audio = os.path.join(output_dir, "drum.wav")
+            # metronome_audio = os.path.join(output_dir, "metronome.wav")
+            # drum_audio = os.path.join(output_dir, "drum.wav")
+            beat_track_audio = os.path.join(output_dir, "beat_track.wav")
             combined_audio = os.path.join(output_dir, "combined_audio.wav")
-
-            st.subheader("Beat Locations")
-            download_text(beat_locations_file)
 
             st.subheader("Waveform")
             display_image(waveform_image)
@@ -92,16 +69,21 @@ def main():
             st.subheader("Spectrogram with Beat Locations")
             display_image(spectrogram_with_beats_image)
 
-            if not use_drum:
-                st.subheader("Metronome Audio")
-                display_audio(metronome_audio, 1)
-
-            if use_drum:
-                st.subheader("Drum Audio")
-                display_audio(drum_audio, 2)
+            # if beat_type == 'Metronome':
+            #     st.subheader("Metronome Audio")
+            #     # display_audio(metronome_audio, 1)
+            #
+            # elif beat_type == 'Drum':
+            #     st.subheader("Drum Audio")
+            st.subheader(beat_type+" Audio")
+                # display_audio(drum_audio, 2)
+            display_audio(beat_track_audio, 1)
 
             st.subheader("Combined Audio")
-            display_audio(combined_audio, 3)
+            display_audio(combined_audio, 2)
+
+            st.subheader("Beat Locations")
+            download_text(beat_locations_file)
 
 if __name__ == "__main__":
     main()
--- .venv/Scripts/beat_detection.py
+++ .venv/Scripts/beat_detection.py
@@ -8,27 +8,28 @@ import librosa.display
 import soundfile as sf
 from BeatNet.BeatNet import BeatNet
 
-def detect_beats(input_file, use_beatnet):
-    if use_beatnet:
+def detect_beats(input_file, detection_method):
+    if detection_method == 'BeatNet':
         estimator = BeatNet(1, mode='offline', inference_model='DBN', plot=[], thread=False)
         Output = estimator.process(input_file)
         beat_times = Output[:,0]
-    else:
+        return beat_times
+
+    elif detection_method == 'Librosa':
         y, sr = librosa.load(input_file)
         tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
         beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+        return beat_times
 
-    return beat_times
-
-def generate_metronome_track(input_file, beat_times, output_dir, use_drum=False):
+def generate_beat_track(input_file, beat_times, output_dir, beat_type):
     y, sr = librosa.load(input_file)
 
     # Create metronome or drum beats
-    if use_drum:
+    if beat_type == 'Drum':
         click_times = beat_times
         click_duration = 0.05  # Duration of each drum beat
         click = np.random.normal(size=int(sr * click_duration))
-    else:
+    elif beat_type == 'Metronome':
         click_times = np.repeat(beat_times, 2)
         click = np.tile([1, -1], len(beat_times))
 
@@ -39,27 +40,51 @@ def generate_metronome_track(input_file, beat_times, output_dir, use_drum=False)
         click_track[start_sample:start_sample + len(click)] += click
 
     # Save metronome or drum track
-    output_path = os.path.join(output_dir, "metronome.wav" if not use_drum else "drum.wav")
+    filename = "beat_track.wav"
+    output_path = os.path.join(output_dir, filename)
     sf.write(output_path, click_track, sr)
+    return click_track
 
 
-def generate_combined_audio(input_file, beat_times, output_dir, use_drum=False):
+def generate_beat_track(input_file, beat_times, output_dir, beat_type):
     y, sr = librosa.load(input_file)
 
-    # Create metronome or drum beats
-    if use_drum:
+    if beat_type == 'Meow' or beat_type == 'Chicken' or beat_type == 'Guitar':
+        meow_file = "C:\\Users\\adars\\PycharmProjects\\BeatVisualizer\\.venv\\Scripts\\" + beat_type + ".wav"
+        # Load the cat meow sound
+        meow, meow_sr = librosa.load(meow_file, sr=sr)
+        # Resample if needed to match the input file's sample rate
+        if meow_sr != sr:
+            meow = librosa.resample(meow, orig_sr=meow_sr, target_sr=sr)
+        click_times = beat_times
+        click = meow
+    elif beat_type == 'Drum':
         click_times = beat_times
         click_duration = 0.05  # Duration of each drum beat
         click = np.random.normal(size=int(sr * click_duration))
-    else:
+    elif beat_type == 'Metronome':
         click_times = np.repeat(beat_times, 2)
         click = np.tile([1, -1], len(beat_times))
 
-    # Create metronome or drum track
+    # Create beat track
     click_track = np.zeros_like(y)
-    for i, click_start in enumerate(click_times):
+    for click_start in click_times:
         start_sample = int(sr * click_start)
-        click_track[start_sample:start_sample + len(click)] += click
+        end_sample = start_sample + len(click)
+        if end_sample > len(click_track):
+            break
+        click_track[start_sample:end_sample] += click
+
+    # Save beat track
+    filename = "beat_track.wav"
+    output_path = os.path.join(output_dir, filename)
+    sf.write(output_path, click_track, sr)
+    return click_track
+
+def generate_combined_audio(input_file, beat_times, output_dir, beat_type):
+    y, sr = librosa.load(input_file)
+
+    click_track = generate_beat_track(input_file, beat_times, output_dir, beat_type)
 
     # Combine original audio with metronome or drum track
     combined_audio = np.vstack((y, click_track[:len(y)]))
@@ -93,20 +118,11 @@ def visualize_waveform(input_file, output_dir,max_duration=30):
 
 
 def visualize_beats(input_file, beat_times, output_dir, max_duration=30):
-    # y, sr = librosa.load(input_file)
-    # plt.figure(figsize=(12, 6))
-    # librosa.display.waveshow(y, sr=sr)
-    # plt.vlines(beat_times, ymin=-1, ymax=1, color='r', linestyle='--', alpha=0.5, label='Beats')
-    # plt.title('Waveform with Beat Locations')
-    # plt.legend()
-    # plt.savefig(os.path.join(output_dir, 'waveform_with_beats.png'))
-    # plt.close()
 
     y, sr = librosa.load(input_file, duration=max_duration)
     plt.figure(figsize=(12, 6))
     librosa.display.waveshow(y, sr=sr)
-    plt.vlines(beat_times[beat_times <= max_duration], ymin=-1, ymax=1, color='r', linestyle='--', alpha=0.5,
-               label='Beats')
+    plt.vlines(beat_times[beat_times <= max_duration], ymin=-1, ymax=1, color='r', linestyle='--', alpha=0.5,label='Beats')
     plt.title('Waveform with Beat Locations (up to 30 seconds)')
     plt.legend()
     plt.savefig(os.path.join(output_dir, 'waveform_with_beats.png'))
@@ -137,40 +153,12 @@ def visualize_spectrogram_with_beats(input_file, beat_times, output_dir, max_dur
     plt.savefig(os.path.join(output_dir, 'spectrogram_with_beats.png'))
     plt.close()
 
-    # # Load only the required portion of the audio file
-    # y, sr = librosa.load(input_file, sr=None, duration=max_duration)
-    #
-    # # Compute the Short-Time Fourier Transform (STFT) with optimized parameters
-    # hop_length = 512  # Number of samples between successive frames
-    # n_fft = 2048  # Length of the FFT window
-    # S = librosa.stft(y, n_fft=n_fft, hop_length=hop_length)
-    #
-    # # Convert the amplitude spectrogram to dB-scaled spectrogram
-    # S_db = librosa.amplitude_to_db(np.abs(S), ref=np.max)
-    #
-    # # Plot the spectrogram with beat times
-    # plt.figure(figsize=(12, 6))
-    # librosa.display.specshow(S_db, sr=sr, hop_length=hop_length, x_axis='time', y_axis='log', cmap='coolwarm')
-    #
-    # # Convert beat times to frames for vertical lines plotting
-    # beat_frames = librosa.time_to_frames(beat_times, sr=sr, hop_length=hop_length)
-    # plt.vlines(beat_times, ymin=0, ymax=sr / 2, color='r', linestyle='--', alpha=0.5, label='Beats')
-    #
-    # plt.colorbar(format='%+2.0f dB')
-    # plt.title('Spectrogram with Beat Locations')
-    # plt.legend(loc='upper right')
-    # plt.tight_layout()
-    #
-    # # Save the figure
-    # plt.savefig(os.path.join(output_dir, 'spectrogram_with_beats.png'))
-    # plt.close()
-
 def main():
     parser = argparse.ArgumentParser(description='Beat detection command line tool')
     parser.add_argument('input_file', help='Input WAV file location')
     parser.add_argument('output_dir', help='Output directory')
-    parser.add_argument('--use_drum', action='store_true', help='Use drum sound instead of metronome')
-    parser.add_argument('--use_beatnet', action='store_true', help='Use beatnet model')
+    parser.add_argument('beat_type', help='Use Drum or Metronome')
+    parser.add_argument('detection_method', help='Use Librosa or Beatnet')
 
     args = parser.parse_args()
 
@@ -178,15 +166,15 @@ def main():
     os.makedirs(args.output_dir, exist_ok=True)
 
     # Perform beat detection
-    beat_times = detect_beats(args.input_file, args.use_beatnet)
+    beat_times = detect_beats(args.input_file, args.detection_method)
 
     # Generate outputs
     visualize_waveform(args.input_file, args.output_dir)
     visualize_beats(args.input_file, beat_times, args.output_dir)
     visualize_spectrogram(args.input_file, args.output_dir)
     visualize_spectrogram_with_beats(args.input_file, beat_times, args.output_dir)
-    generate_metronome_track(args.input_file, beat_times, args.output_dir, args.use_drum)
-    generate_combined_audio(args.input_file, beat_times, args.output_dir, args.use_drum)
+    generate_beat_track(args.input_file, beat_times, args.output_dir, args.beat_type)
+    generate_combined_audio(args.input_file, beat_times, args.output_dir, args.beat_type)
     generate_txt(beat_times, args.output_dir)
 
     print("Beat detection and output generation completed.")
